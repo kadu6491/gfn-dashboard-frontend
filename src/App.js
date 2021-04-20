18a01a7192dfa8813041012ac11ad9450b8eb86c
Kofi Adutwum <kadut001@odu.edu>
2021-04-20 13:34:50 -0400

    deploy try 3

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h5>Good for nothing</h5>
+      <h5>Good for nothing - nice weather today is</h5>
     </div>
   );
 }
