dc146455c2b4111e745d3ebcaeed28f057df2cc9
Kofi Adutwum <kadut001@odu.edu>
2021-05-21 11:05:31 -0400

    cleared caches

--- src/navigation/style.js
+++ src/navigation/style.js
@@ -3,71 +3,71 @@ import {makeStyles} from '@material-ui/core/styles'
 const drawerWidth = 240;
 
 const useStyles = makeStyles((theme) => ({
-    // root: {
-    //     display: 'flex',
-    //     backgroundColor: "#eee",
-    //     position: "absolute",
-    //     height: "100%",
-    //     width: "100%",
-    // },
-    // drawer: {
-    //     [theme.breakpoints.up('md')]: {
-    //       width: drawerWidth,
-    //       flexShrink: 0,
-    //     },
-    //   },
-    //   appBar: {
-    //     [theme.breakpoints.up('md')]: {
-    //       width: `calc(100% - ${drawerWidth}px)`,
-    //       marginLeft: drawerWidth,
-    //     },
-    //     backgroundColor: "#0c2233",
-    //   },
-    // menuButton: {
-    //     marginRight: theme.spacing(2),
-    //     [theme.breakpoints.up('md')]: {
-    //         display: 'none',
-    //     },
-    // },
-    // toolbar: theme.mixins.toolbar,
-    // drawerPaper: {
-    //     width: drawerWidth,
-    //     backgroundColor: '#0c2233',
-    // },
-    // content: {
-    //     flexGrow: 1,
-    //     padding: theme.spacing(3),
-    //     [theme.breakpoints.down('xs')]: {
-    //         padding: theme.spacing(1),
-    //         marginTop: "-5px",
-    //     },
-    // },
-    // navTool: {
-    //     // position: "absolute",
-    //     // height: "auto",
-    //     // width: "100%",
-    //     // backgroundColor: '#0c2233',
-    //     color: "white",
-    // },
-    // listNav: {
-    //     "&:hover": {
-    //         backgroundColor: "#065471",
-    //         opacity: "0.7",
-    //     },
-    // },
-    // active: {
-    //     backgroundColor: "red",
-    // },
-    // grow: {
-    //     flexGrow: 1,
-    //   },
-    // nestedxs: {
-    //     paddingLeft: theme.spacing(4),
-    //     "&:hover": {
-    //         backgroundColor: "#065471",
-    //         opacity: "0.7",
-    //     },
-    //   },
+    root: {
+        display: 'flex',
+        backgroundColor: "#eee",
+        position: "absolute",
+        height: "100%",
+        width: "100%",
+    },
+    drawer: {
+        [theme.breakpoints.up('md')]: {
+          width: drawerWidth,
+          flexShrink: 0,
+        },
+      },
+      appBar: {
+        [theme.breakpoints.up('md')]: {
+          width: `calc(100% - ${drawerWidth}px)`,
+          marginLeft: drawerWidth,
+        },
+        backgroundColor: "#0c2233",
+      },
+    menuButton: {
+        marginRight: theme.spacing(2),
+        [theme.breakpoints.up('md')]: {
+            display: 'none',
+        },
+    },
+    toolbar: theme.mixins.toolbar,
+    drawerPaper: {
+        width: drawerWidth,
+        backgroundColor: '#0c2233',
+    },
+    content: {
+        flexGrow: 1,
+        padding: theme.spacing(3),
+        [theme.breakpoints.down('xs')]: {
+            padding: theme.spacing(1),
+            marginTop: "-5px",
+        },
+    },
+    navTool: {
+        // position: "absolute",
+        // height: "auto",
+        // width: "100%",
+        // backgroundColor: '#0c2233',
+        color: "white",
+    },
+    listNav: {
+        "&:hover": {
+            backgroundColor: "#065471",
+            opacity: "0.7",
+        },
+    },
+    active: {
+        backgroundColor: "red",
+    },
+    grow: {
+        flexGrow: 1,
+      },
+    nestedxs: {
+        paddingLeft: theme.spacing(4),
+        "&:hover": {
+            backgroundColor: "#065471",
+            opacity: "0.7",
+        },
+      },
 }));
 
 export default useStyles
